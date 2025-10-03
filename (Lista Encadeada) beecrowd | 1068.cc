524c52beea6daa4ceecb6a2302d7e40ad47de245
Gabriel-Moreno-Miranda-da-Silva <eddysite69@outlook.com>
2025-10-02 21:05:26 -0300

    Update and rename Incompleto (Lista Encadeada) beecrowd | 1068.cc to (Lista Encadeada) beecrowd | 1068.cc

--- (Lista Encadeada) beecrowd | 1068.cc
+++ (Lista Encadeada) beecrowd | 1068.cc
@@ -12,18 +12,21 @@ void Insere (string F1, Celula *p1)
 {
 	Celula *nova;
 	nova = new (Celula);
-  //trocar o ponteiro nova->Contudo = F1 para um string.copy
 	nova->Conteudo = F1;
 	nova->seg = p1->seg;
 	p1->seg = nova;
-};
+}
 
-void Imprima (Celula *lst)
+int Desempilha (Celula *p)
 {
-	Celula *p;
-	for (p = lst; p != NULL; p = p->seg) {
-		cout << p->Conteudo;
+	if (p->seg == NULL) {
+		return 0; // Retorna 0 se for vazio
 	}
+	Celula *q;
+	q = p->seg;
+	p->seg = q->seg;
+	delete q;
+	return 1;
 }
 
 int main()
@@ -33,19 +36,29 @@ int main()
 	//criando a cabeca da Pilha
 	Celula *Cabeca = new(Celula);
 	Cabeca->seg = NULL;
-	
+
 	//repete a analise dos elementos ate acabar todos os elementos da lista
 	while (getline(cin, F))
 	{
+	    bool Correto = 1;
 		for(int i = 0 ; i < F.length(); i++) {
 			if (F[i]=='(') {
-				Insere("(",Cabeca->seg);
+				Insere("(",Cabeca);
 			}
 			if (F[i]==')') {
-				Insere(")",Cabeca->seg);
+				if(Desempilha(Cabeca)==0){
+				    Correto = 0;
+				    break;
+				}
 			}
 		}
-        Imprima (Cabeca->seg);
+		if(Desempilha(Cabeca)!=0){
+		    Correto = 0;
+		}
+		if(Correto==1){
+		    cout << "correct\n";
+		} else cout << "incorrect\n";
+        
 	}
 
 	return 0;
